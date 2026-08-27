0ae02e3bedf1d61f823c93d196edcb604d4a7905
fiston <kellydigitalmarketing2@gmail.com>
2026-08-27 22:28:58 +0200

    extrernal links removal
    linkedin and twiter extrtnal links removed from the footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -133,7 +133,7 @@ function App() {
           </ul>
           <div className="flex items-center gap-1.5 sm:gap-4">
             <button onClick={() => setCurrentPage('join')} className="inline-block py-1.5 px-3 sm:py-2.5 sm:px-6 rounded-[50px] font-semibold cursor-pointer transition-all duration-300 bg-primary-blue text-white-main hover:bg-secondary-blue hover:-translate-y-0.5 hover:shadow-[0_10px_20px_rgba(0,102,255,0.2)] text-[10px] sm:text-sm">Join Club</button>
-            <button onClick={() => setCurrentPage('shop')} className="inline-block py-1.5 px-3 sm:py-2.5 sm:px-6 rounded-[50px] font-semibold cursor-pointer transition-all duration-300 bg-primary-blue text-white-main hover:bg-secondary-blue hover:-translate-y-0.5 hover:shadow-[0_10px_20px_rgba(0,102,255,0.2)] text-[10px] sm:text-sm">Shop now!</button>
+            <button onClick={() => setCurrentPage('shop')} className="inline-block py-1.5 px-3 sm:py-2.5 sm:px-6 rounded-[50px] font-semibold cursor-pointer transition-all duration-300 bg-primary-blue text-white-main hover:bg-secondary-blue hover:-translate-y-0.5 hover:shadow-[0_10px_20px_rgba(0,102,255,0.2)] text-[10px] sm:text-sm">Shop now!</button> 
             
             {/* Hamburger Burger Button */}
             <button 
